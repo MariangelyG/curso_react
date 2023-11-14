681c08a7528ed12ad63a51dfe54d50ff4c2bf26e
MariangelyG <mariangelygonzalez90@gmail.com>
2023-11-14 15:42:38 -0300

    "JournalLayout y JournalPage"

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,12 +1,17 @@
 
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 
 export const JournalPage = () => {
   return (
-    <>
-    <Typography variant="h1">JournalPage</Typography>
+    <JournalLayout>
+
+      <Typography> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor iste animi laudantium dicta fugit quas nobis repudiandae voluptas expedita odio magni consectetur nam consequuntur ut itaque quae, tempore deserunt provident!</Typography>
    
-    </>
+   {/* NothingSelected */}
+    {/* NoteView */}
+
+    </JournalLayout>
   )
 }
