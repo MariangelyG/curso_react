befb7e89c895a7db107e074c7670551d142d5fc9
MariangelyG <mariangelygonzalez90@gmail.com>
2023-11-15 21:09:30 -0300

    "Boton flotante"

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,7 +1,8 @@
 
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../view"
+import { AddOutlined } from "@mui/icons-material"
 
 
 export const JournalPage = () => {
@@ -10,8 +11,24 @@ export const JournalPage = () => {
 
       {/* <Typography>Lorem ipsum, gcnjgfjff fcffhfg ffdgj fdjg dolor sit amet consectetur adipisicing elit.</Typography> */}
    
-  {/* <NothingSelectedView/> */}
-    <NoteView/>
+  <NothingSelectedView/>
+    {/* <NoteView/> */}
+
+    <IconButton
+      size="large"
+      sx={{
+        color:'white',
+        backgroundColor:'error.main',
+        ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+        position: 'fixed',
+        right: 50,
+        bottom: 50
+      }}
+    >
+
+      <AddOutlined sx={{fontSize: 30}}/>
+
+    </IconButton>
 
     </JournalLayout>
   )
